b99dfc01bcdc20d5ba1eb694e51d0873e88ec0a6
conciseways <andy@concise-ways.com>
2025-02-06 11:00:13 -0600

    added types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,10 @@
+export type Verb = {
+    root: string; // The root of the verb
+    conjugation: string; // The conjugation of the verb (required)
+    definition: string; // The definition of the verb (required)
+};
+
+
 export type Noun = {
     root: string; // The root of the noun
     type: "noun"; // The type of the word
@@ -5,3 +12,6 @@ export type Noun = {
     definition: string; // The definition of the noun
     declension?: number; // The declension number (optional)
 };
+
+export type Vocabulary = (Noun | Verb)[];
+
